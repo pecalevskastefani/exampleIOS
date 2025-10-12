13c281ae59203c479c5baa9afc9f5eab8670b2d6
Stefani Pecalevska <stefani.pecalevska@netcetera.com>
2025-10-12 17:43:51 +0200

    initial push

---
+++ exampleIOS/exampleIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  exampleIOSApp.swift
+//  exampleIOS
+//
+//  Created by Stefani Pecalevska on 12.10.25.
+//
+
+import SwiftUI
+
+@main
+struct exampleIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ exampleIOSTests/exampleIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  exampleIOSTests.swift
+//  exampleIOSTests
+//
+//  Created by Stefani Pecalevska on 12.10.25.
+//
+
+import Testing
+@testable import exampleIOS
+
+struct exampleIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
